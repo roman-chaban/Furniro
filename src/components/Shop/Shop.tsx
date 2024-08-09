4cff331b0b50482f9fc25eba57a8da7e45fd1a59
Roman Chaban <roman.chaban1001@gmail.com>
2024-08-09 03:02:34 -0800

    Fixed Shop page bugs

--- src/components/Shop/Shop.tsx
+++ src/components/Shop/Shop.tsx
@@ -1,19 +1,19 @@
-import type { FC } from "react";
-import styles from "./Shop.module.scss";
-import { ProductHeader } from "./ProductHeader";
-import { OurProducts } from "../OurProducts/OurProducts";
-import { Pagination } from "./Pangination";
-import { Benefits } from "./Benefits";
-import { Poster } from "../UI components/Poster/Poster";
+import type { FC } from 'react';
+import styles from './Shop.module.scss';
+import { ProductHeader } from './ProductHeader';
+import { OurProducts } from '../OurProducts/OurProducts';
+import { Pagination } from './Pangination';
+import { Benefits } from './Benefits';
+import { Poster } from '../UI components/Poster/Poster';
 
 export const ShopComponent: FC = () => {
   return (
     <section className={styles.shopSection}>
-      <Poster title={"Shop"} linkPrev={"Home"} linkNext={"Shop"} />
+      <Poster title={'Shop'} linkPrev={'Home'} linkNext={'Shop'} />
       <div className={styles.productSection}>
         <ProductHeader />
         <div className={styles.productList}>
-          <OurProducts isShop={true} />
+          <OurProducts isShop={true} className={''} productsTitle="" />
           <Pagination />
         </div>
       </div>
